842a5a0023aa53e4d7b17139d2ab0da5a908be69
S65784 <summerholiday65784@outlook.com>
2025-11-09 17:01:03 +0800

    add dash

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -13,8 +15,19 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(11.24)
-            .forwardZeroPowerAcceleration(33.81)
-            .lateralZeroPowerAcceleration(-56);
+            .forwardZeroPowerAcceleration(-30.81)
+            .lateralZeroPowerAcceleration(-56)
+
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.04, 0, 0, 0.01))
+//            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.1,0,0.01,0))
+
+            .headingPIDFCoefficients(new PIDFCoefficients(1, 0, 0.01, 0.03))
+            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(2,0,0.02,0.02))
+
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.1,0.0,0.01,0.6,0.0))
+            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(0.1,0,0.01,0.6,0.01))
+            ;
+
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
             .rightFrontMotorName("RightFrontDrive")
@@ -25,12 +38,12 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .xVelocity(85)
-            .yVelocity(64);
+            .xVelocity(78.5)
+            .yVelocity(64.5);
     public static ThreeWheelConstants localizerConstants = new ThreeWheelConstants()
             .forwardTicksToInches(.001989436789)
             .strafeTicksToInches(.001989436789)
-            .turnTicksToInches(.001989436789)
+            .turnTicksToInches(.0020289436789)
             .leftPodY(2.8937)
             .rightPodY(0)
             .strafePodX(-2.362)
@@ -41,7 +54,7 @@ public class Constants {
             .rightEncoderDirection(Encoder.REVERSE)
             .strafeEncoderDirection(Encoder.FORWARD)
             .turnTicksToInches(0.00212);
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 0.4, 0.2);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/shooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/shooterTest.java
@@ -20,12 +20,15 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
 @TeleOp
@@ -41,6 +44,7 @@ public class shooterTest extends LinearOpMode {
     private DcMotorEx shooter = null;
     private Servo ls = null;
     private Servo rs = null;
+    private IMU imu = null;
 
     //private Servo block = null;
     private boolean lastYState = false;
@@ -63,6 +67,11 @@ public class shooterTest extends LinearOpMode {
         ls  = hardwareMap.get(Servo.class, "ls");
         rs  = hardwareMap.get(Servo.class, "rs");
 
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new com.qualcomm.hardware.rev.RevHubOrientationOnRobot(
+                com.qualcomm.hardware.rev.RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                com.qualcomm.hardware.rev.RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        imu.initialize(parameters);
         //block = hardwareMap.get(Servo.class, "Block");
 
         leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -89,21 +98,26 @@ public class shooterTest extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
+        imu.resetYaw();
+
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             ls.setPosition(0.4);
             rs.setPosition(0.6);
 
             double max;
-
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
             double axial   = -gamepad1.left_stick_y;
             double lateral =  gamepad1.left_stick_x;
             double yaw     =  gamepad1.right_stick_x;
 
-            double leftFrontPower  = axial + lateral + yaw;
-            double rightFrontPower = axial - lateral - yaw;
-            double leftBackPower   = axial - lateral + yaw;
-            double rightBackPower  = axial + lateral - yaw;
+            double newAxial = axial * Math.cos(botHeading) - lateral * Math.sin(botHeading);
+            double newLateral = axial * Math.sin(botHeading) + lateral * Math.cos(botHeading);
+
+            double leftFrontPower  = newAxial + newLateral + yaw;
+            double rightFrontPower = newAxial - newLateral - yaw;
+            double leftBackPower   = newAxial - newLateral + yaw;
+            double rightBackPower  = newAxial + newLateral - yaw;
 
             max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
             max = Math.max(max, Math.abs(leftBackPower));
@@ -124,19 +138,19 @@ public class shooterTest extends LinearOpMode {
 
             if(gamepad1.a){
                 intake.setPower(0.8);
-                //blender.setPower(0.55);
+                blender.setPower(0.6);
                 shooter.setPower(0);
             }
 
-            if(gamepad1.b){
-                intake.setPower(-0.8);
-                //blender.setPower(-0.55);
-                shooter.setPower(0);
-            }
-            if(gamepad1.square){
-                shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                sleep(300);
-            }
+//            if(gamepad1.b){
+//                intake.setPower(-0.8);
+//                //blender.setPower(-0.55);
+//                shooter.setPower(0);
+//            }
+//            if(gamepad1.square){
+//                shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//                sleep(300);
+//            }
 
             boolean currentYState = gamepad1.y;
             if (currentYState && !lastYState && !state) {
@@ -155,7 +169,6 @@ public class shooterTest extends LinearOpMode {
                 //sleep(100);
                 state = false;
             }
-
             lastYState = currentYState;
 
             if(gamepad1.x){
@@ -165,6 +178,10 @@ public class shooterTest extends LinearOpMode {
                 shooter.setPower(-1);
             }else if(shooterDecelarateTime.seconds()<1.1){
                 shooter.setPower(0);
+            }
+
+            if (gamepad1.options) {
+                imu.resetYaw();
             }
 
 //            if(gamepad1.dpad_down){//低
