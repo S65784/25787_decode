aa86a63123edad960a95be2b68f6ec5b487462d9
S65784 <summerholiday65784@outlook.com>
2025-11-14 08:43:33 +0800

    modify RedAutoDos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Red/RedAutoDos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Red/RedAutoDos.java
@@ -42,55 +42,39 @@ public class RedAutoDos extends OpMode {
     // Define Poses
     private final Pose startPose = new Pose(84.8, 8.8, Math.toRadians(90));
 
-    private final Pose controlScorePose = new Pose(93, 62.9, Math.toRadians(32));
+    private final Pose controlScorePose = new Pose(93.06631989596879, 62.91807542262679, Math.toRadians(0));
     private final Pose scorePose = new Pose(87.2, 81.8, Math.toRadians(40));
-//    private final Pose scorePose1 = new Pose(113, 111, Math.toRadians(32));//
+
     private final Pose controlScorePose2 = new Pose(83.51625487646294, 62.73081924577374, Math.toRadians(40));
     private final Pose scorePose2 = new Pose(87.2, 81.8, Math.toRadians(40));
+
     private final Pose controlScorePose3 = new Pose(84, 55, Math.toRadians(40));
-    private final Pose scorePose3 = new Pose(113, 109, Math.toRadians(40));
+    private final Pose scorePose3 = new Pose(87.2, 81.87, Math.toRadians(40));
 
 
-//    private final Pose controlPickup1Ready = new Pose(92, 97, Math.toRadians(0));
-//    private final Pose pickup1Ready = new Pose(getPointPreX, Point1Y, Math.toRadians(0));
-//    private final Pose pickup1Pose = new Pose(getPointX, Point1Y, Math.toRadians(0));
 
-    //private final Pose controlPickup2Ready = new Pose(76.6, 61.9, Math.toRadians(0));
     private final Pose pickup2Ready = new Pose(103.17815344603382, 59.36020806241872, Math.toRadians(0));
     private final Pose pickup2Pose = new Pose(getPointX, 59.36020806241872, Math.toRadians(0));
 
     private final Pose controlTheGate = new Pose(119.84395318595578, 68.34850455136541, Math.toRadians(0));
     private final Pose theGate = new Pose(129.01950585175553, 68.16124837451235, Math.toRadians(0));
 
-    //private final Pose controlPickup3Ready = new Pose(84, 55, Math.toRadians(0));
-    private final Pose pickup3Ready = new Pose(getPointPreX, Point3Y, Math.toRadians(0));
+    private final Pose pickup3Ready = new Pose(101.30559167750326, Point3Y, Math.toRadians(0));
     private final Pose pickup3Pose = new Pose(131, Point3Y, Math.toRadians(0));
 
-    private Path scorePreload, runto1, runto2, runto3, park;
-    private PathChain grabPickup1, grabPickup2, grabPickup3, scorePickup1, scorePickup2, scorePickup3, runTheGate;
+    private Path scorePreload, runto2, runto3, park;
+    private PathChain grabPickup2, grabPickup3, scorePickup2, scorePickup3, runTheGate;
 
     public void buildPaths() {
 
-        scorePreload = new Path(new BezierLine(startPose, scorePose));
+        scorePreload = new Path(new BezierCurve(startPose, controlScorePose, scorePose));
         scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
-
-//        runto1 = new Path(new BezierCurve(scorePose,controlPickup1Ready,pickup1Ready));
-//        runto1.setLinearHeadingInterpolation(scorePose.getHeading(), pickup1Ready.getHeading());
 
         runto2 = new Path(new BezierLine(scorePose,pickup2Ready));
         runto2.setLinearHeadingInterpolation(scorePose.getHeading(), pickup2Ready.getHeading());
 
         runto3 = new Path(new BezierLine(scorePose,pickup3Ready));
         runto3.setLinearHeadingInterpolation(scorePose.getHeading(), pickup3Ready.getHeading());
-
-//        grabPickup1 = follower.pathBuilder()
-//                .addPath(new BezierLine(pickup1Ready,pickup1Pose))
-//                .setLinearHeadingInterpolation(pickup1Ready.getHeading(), pickup1Pose.getHeading())
-//                .build();
-//        scorePickup1 = follower.pathBuilder()
-//                .addPath(new BezierLine(pickup1Pose, scorePose1))
-//                .setLinearHeadingInterpolation(pickup1Pose.getHeading(), scorePose1.getHeading())
-//                .build();
 
         grabPickup2 = follower.pathBuilder()
                 .addPath(new BezierLine(pickup2Ready,pickup2Pose))
@@ -114,10 +98,6 @@ public class RedAutoDos extends OpMode {
                 .setLinearHeadingInterpolation(pickup3Pose.getHeading(), scorePose3.getHeading())
                 .build();
 
-//        park = new Path(new BezierCurve(new Point(scorePose),
-//                new Point(parkControlPose),
-//                new Point(parkPose)));
-//        park.setLinearHeadingInterpolation(scorePose.getHeading(), parkPose.getHeading());
     }
 
     public void autonomousPathUpdate() {
